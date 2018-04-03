10479816f840cd8a4cb5ae6c9697ffc1fcf701d9
Abdullah <abdullah.alwaheb@gmail.com>
2018-04-03 14:58:45 -0500

    updated

--- promise-exercise/run.js
+++ promise-exercise/run.js
@@ -10,47 +10,45 @@ urls = [
     'https://en.wikipedia.org/wiki/Google_Chrome'
 ];
 
-// const pageLink = {
-//     method: 'GET',
-//     urls =[
-//         'https://en.wikipedia.org/wiki/Futures_and_promises',
-//         'https://en.wikipedia.org/wiki/Continuation-passing_style',
-//         'https://en.wikipedia.org/wiki/JavaScript',
-//         'https://en.wikipedia.org/wiki/Node.js',
-//         'https://en.wikipedia.org/wiki/Google_Chrome'
-//     ]
-// };
+// using promise-request
+const htmlPage = {
+    method: 'GET',
+    url: urls[3]
+};
 
-// rp(pageLink)
-//     .then(function (res) {
-//         console.log(urls[1]);
-//     })
-//     .catch(function (err) {
-//         console.log(err);
-//     });
-
-var l1 = new Promise(function (resolve, reject) {
-    resolve(urls[0]);
-});
-var l2 = new Promise(function (resolve, reject) {
-    resolve(urls[1]);
-});
-var l3 = new Promise(function (resolve, reject) {
-    resolve(urls[2]);
-});
-var l4 = new Promise(function (resolve, reject) {
-    resolve(urls[3]);
-});
-var l5 = new Promise(function (resolve, reject) {
-    resolve(urls[4]);
-});
-Promise.all([l1,l2,l3,l4,l5])
-    .then(function(data){
+rp(htmlPage)
+    .then(function (data) {
         console.log(data);
     })
-    .catch(function(err){
+    .catch(function (err) {
         console.log(err);
     });
+
+
+// promise.all
+// var l1 = new Promise(function (resolve, reject) {
+//     resolve(urls[0]);
+// });
+// var l2 = new Promise(function (resolve, reject) {
+//     resolve(urls[1]);
+// });
+// var l3 = new Promise(function (resolve, reject) {
+//     resolve(urls[2]);
+// });
+// var l4 = new Promise(function (resolve, reject) {
+//     resolve(urls[3]);
+// });
+// var l5 = new Promise(function (resolve, reject) {
+//     resolve(urls[4]);
+// });
+
+// Promise.all([l1,l2,l3,l4,l5])
+//     .then(function(data){
+//         console.log(data);
+//     })
+//     .catch(function(err){
+//         console.log(err);
+//     });
 
 
 var server = app.listen(3000, function () {
